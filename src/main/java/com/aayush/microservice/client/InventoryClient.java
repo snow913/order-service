c84bbc00bad06598bb6957f8401ba29e2745b9aa
snow913 <aayushbhurtelaws+github@gmail.com>
2024-09-02 14:58:44 -0500

    Using REST Client intead of SpringFein in Order Service.

--- src/main/java/com/aayush/microservice/client/InventoryClient.java
+++ src/main/java/com/aayush/microservice/client/InventoryClient.java
@@ -1,18 +1,23 @@
 package com.aayush.microservice.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "http://localhost:8082")
+
 public interface InventoryClient {
 
-    // define what kind of http client is going to call in this request
+ /*         New way
+ //@FeignClient(value = "inventory", url = "http://localhost:8082")
+  // define what kind of http client is going to call in this request
     @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam int quantity);
 
     // this is only getmapping, we could also do same for post put or delete method
+*/
+
+    @GetExchange("/api/inventory")
+    boolean isInStock(@RequestParam String skuCode, @RequestParam int quantity);
+// Using GetExchange Annotation, Spring will take care of all the url and mapping that we implemented above.
+
 
 }
